562d1004ff4c1e5e429fd90679c4d8987fe51ec3
poznerrr <pozner@gmail.com>
2022-12-04 18:17:14 +0300

    feat: add player and game classes

--- script.js
+++ script.js
@@ -15,7 +15,20 @@ window.addEventListener('load', function () {
 
   }
   class Player {
-
+    constructor(game) {
+      this.game = game;
+      this.width = 120;
+      this.height = 190;
+      this.x = 20;
+      this.y = 100;
+      this.speedY = 0;
+    }
+    update() {
+      this.y += this.speedY;
+    }
+    draw(context) {
+      context.fillRect(this.x, this.y, this.width, this.height)
+    }
   }
   class Enemy {
 
@@ -30,6 +43,19 @@ window.addEventListener('load', function () {
 
   }
   class Game {
-
+    constructor(width, height) {
+      this.width = width;
+      this.height = height;
+      this.player = new Player(this);
+    }
+    update() {
+      this.player.update(context);
+    }
+    draw() {
+      this.player.draw(context);
+    }
   }
+
+
+  const game = new Game(canvas.width, canvas.height);
 });
