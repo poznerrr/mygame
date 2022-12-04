e96d4d74c1eaa6c17d098620d2be336398d20b4d
poznerrr <pozner@gmail.com>
2022-12-04 12:28:41 +0300

    feat: add basic javascript  setup

---
+++ script.js
@@ -0,0 +1,7 @@
+window.addEventListener('load', function () {
+  //canvas setup
+  const canvas = this.document.getElementById('canvas1');
+  const ctx = canvas.getContext('2d');
+  canvas.height = 500;
+  canvas.width = 1500;
+});
