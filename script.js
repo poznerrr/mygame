e230ed80548207f29d908e9db399cebf223fbcd4
poznerrr <pozner@gmail.com>
2022-12-04 19:06:19 +0300

    feat: realize animate loop function

--- script.js
+++ script.js
@@ -49,13 +49,20 @@ window.addEventListener('load', function () {
       this.player = new Player(this);
     }
     update() {
-      this.player.update(context);
+      this.player.update();
     }
-    draw() {
+    draw(context) {
       this.player.draw(context);
     }
   }
 
 
   const game = new Game(canvas.width, canvas.height);
+  //animation loop
+  function animate() {
+    game.update();
+    game.draw(ctx);
+    requestAnimationFrame(animate);
+  }
+  animate()
 });
