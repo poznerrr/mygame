238e5e0a66dc34184b653272185cd59177917323
poznerrr <pozner@gmail.com>
2022-12-04 12:57:09 +0300

    feat: add required classes

--- script.js
+++ script.js
@@ -4,4 +4,32 @@ window.addEventListener('load', function () {
   const ctx = canvas.getContext('2d');
   canvas.height = 500;
   canvas.width = 1500;
+
+  class InputHandler {
+
+  }
+  class Projectile {
+
+  }
+  class Particle {
+
+  }
+  class Player {
+
+  }
+  class Enemy {
+
+  }
+  class Layer {
+
+  }
+  class Background {
+
+  }
+  class UI {
+
+  }
+  class Game {
+
+  }
 });
